99573e4237a4038a8d328db4422a6236d86129ea
Raj Kamal Mishra <rajkamalmishra700@gmail.com>
2025-03-25 00:57:29 +0530

    added other files also

---
+++ src/test/java/com/learningSpringSecurity/LearnSpringSecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpringSecurity.LearnSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
